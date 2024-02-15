85d37c56628be26c702863da8a984960517e1ff8
taimi06 <112554212+taimi06@users.noreply.github.com>
2024-02-15 22:45:31 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,60 @@
+
+# Github-Asana action
+
+This action integrates asana with github.
+
+### Prerequisites
+
+- Asana account with the permission on the particular project you want to integrate with.
+- Must provide the task url in the PR description.
+
+## Inputs
+
+### `asana-pat`
+
+**Required** Your public access token of asana, you can find it in [asana docs](https://developers.asana.com/docs/#authentication-basics).
+
+### `trigger-phrase`
+
+**Required** Prefix before the task i.e ASANA TASK: https://app.asana.com/1/2/3/. For special characters in the trigger phrase refer to the examples.
+
+### `task-comment`
+
+**Optional** If any comment is provided, the action will add a comment to the specified asana task with the text & pull request link.
+
+### `targets`
+
+**Optional** JSON array of objects having project and section where to move current task. Move task only if it exists in target project. e.g 
+```yaml
+targets: '[{"project": "Backlog", "section": "Development Done"}, {"project": "Current Sprint", "section": "In Review"}]'
+```
+if you don't want to move task omit `targets`.
+
+## Sample PR Description
+``
+**Asana Task:** [Task Name](https://app.asana.com/0/1/2)
+``
+
+## Examples
+
+#### Without special characters:
+
+```yaml
+uses: insurify/github-asana-action@v1.0.6
+with:
+  asana-pat: 'Your PAT'
+  task-comment: 'View Pull Request Here: '
+  trigger-phrase: 'Asana Task:'
+  targets: '[{"project": "Backlog", "section": "Development Done"}, {"project": "Current Sprint", "section": "In Review"}]'
+```
+
+#### With special characters:
+
+```yaml
+uses: insurify/github-asana-action@v1.0.6
+with:
+  asana-pat: 'Your PAT'
+  task-comment: 'View Pull Request Here: '
+  trigger-phrase: "\\*\\*Asana Task:\\*\\*"
+  targets: '[{"project": "Backlog", "section": "Development Done"}, {"project": "Current Sprint", "section": "In Review"}]'
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const Asana = require('asana');
+
+async function asanaOperations(
+  asanaPAT,
+  targets,
+  taskId,
+  taskComment
+) {
+  try {
+    let client = Asana.ApiClient.instance;
+    let token = client.authentications['token'];
+    token.accessToken = asanaPAT;
+    let tasksApiInstance = new Asana.TasksApi();
+    let sectionsApiInstance = new Asana.SectionsApi();
+    let storiesApiInstance = new Asana.StoriesApi();
+
+    const task = await tasksApiInstance.getTask(taskId);
+
+    targets.forEach(async target => {
+      let targetProject = task.data.projects.find(project => project.name === target.project);
+      if (targetProject) {
+        let targetSection = await sectionsApiInstance.getSectionsForProject(targetProject.gid)
+          .then(sections => sections.data.find(section => section.name === target.section));
+        if (targetSection) {
+          await sectionsApiInstance.addTaskForSection(targetSection.gid, { body: { data: { task: taskId } } });
+          core.info(`Moved to: ${target.project}/${target.section}`);
+        } else {
+          core.error(`Asana section ${target.section} not found.`);
+        }
+      } else {
+        core.info(`This task does not exist in "${target.project}" project`);
+      }
+    });
+
+    if (taskComment) {
+      await storiesApiInstance.createStoryForTask({ data: { text: taskComment } }, taskId)
+      core.info('Added the pull request link to the Asana task.');
+    }
+  } catch (ex) {
+    console.error(ex.value);
+  }
+}
+
+try {
+  const ASANA_PAT = core.getInput('asana-pat'),
+    TARGETS = core.getInput('targets'),
+    TRIGGER_PHRASE = core.getInput('trigger-phrase'),
+    TASK_COMMENT = core.getInput('task-comment'),
+    PULL_REQUEST = github.context.payload.pull_request,
+    REGEX = new RegExp(
+      `${TRIGGER_PHRASE} *\\[(.*?)\\]\\(https:\\/\\/app.asana.com\\/(\\d+)\\/(?<project>\\d+)\\/(?<task>\\d+).*?\\)`,
+      'g'
+    );
+  let taskComment = null,
+    targets = TARGETS? JSON.parse(TARGETS) : [],
+    parseAsanaURL = null;
+
+  if (!ASANA_PAT){
+    throw({message: 'ASANA PAT Not Found!'});
+  }
+  if (TASK_COMMENT) {
+    taskComment = `${TASK_COMMENT} ${PULL_REQUEST.html_url}`;
+  }
+  while ((parseAsanaURL = REGEX.exec(PULL_REQUEST.body)) !== null) {
+    let taskId = parseAsanaURL.groups.task;
+    if (taskId) {
+      asanaOperations(ASANA_PAT, targets, taskId, taskComment);
+    } else {
+      core.info(`Invalid Asana task URL after the trigger phrase ${TRIGGER_PHRASE}`);
+    }
+  }
+} catch (error) {
+  core.error(error.message);
+}
